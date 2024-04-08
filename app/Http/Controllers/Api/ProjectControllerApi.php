82e32e70a8eb8c9e42905a637dcb6d8dd63d018d
BernardoCosmi <bernardo.daddo90@gmail.com>
2024-04-08 17:51:03 +0200

    feat: vue router show

--- app/Http/Controllers/Api/ProjectControllerApi.php
+++ app/Http/Controllers/Api/ProjectControllerApi.php
@@ -20,4 +20,20 @@ class ProjectControllerApi extends Controller
             'projects' => $projects,
         ]);
     }
+
+    public function show( $slug ){
+        $project = Project::with('type','technologies')->where('slug', $slug)->first();
+
+        if($project){
+            return response()->json([
+                'success' => true,
+                'project' => $project,
+            ]);
+        }else{
+            return response()->json([
+                'success' => false,
+                'error' => 'Progetto non trovato',
+            ]);
+        }
+    }
 }
